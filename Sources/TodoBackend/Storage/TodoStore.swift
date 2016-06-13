6eeccd76db28e95e4ef611c112ab277f87be364d
Dan Appel <dan.appel00@gmail.com>
2016-06-12 23:17:33 -0700

    Fix file naming error

--- Sources/TodoBackend/Storage/TodoStore.swift
+++ Sources/TodoBackend/Storage/TodoStore.swift
@@ -22,38 +22,14 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-final class InMemoryTodoStore: TodoStore {
-    private var idCounter = 0
-    private var storage = [Int:Entity<Todo>]()
+protocol TodoStore {
+    func get(id: Int) -> Entity<Todo>?
+    func getAll() -> [Entity<Todo>]
 
-    func getAll() -> [Entity<Todo>] {
-        return Array(storage.values)
-    }
+    func insert(todo: Todo) -> Entity<Todo>
 
-    func get(id: Int) -> Entity<Todo>? {
-        return storage[id]
-    }
+    func update(id: Int, todo: Todo) -> Entity<Todo>
 
-    func insert(todo: Todo) -> Entity<Todo> {
-        defer { idCounter += 1 }
-        let entity = Entity(id: idCounter, url: "\(apiRoot)\(idCounter)", item: todo)
-        storage[idCounter] = entity
-        return entity
-    }
-
-    func update(id: Int, todo: Todo) -> Entity<Todo> {
-        let entity = Entity(id: id, item: todo)
-        storage[id] = entity
-        return entity
-    }
-
-    func remove(id: Int) -> Entity<Todo>? {
-        defer { storage[id] = nil }
-        return storage[id]
-    }
-
-    func clear() -> [Entity<Todo>] {
-        defer { self.storage = [:] }
-        return Array(storage.values)
-    }
+    func remove(id: Int) -> Entity<Todo>?
+    func clear() -> [Entity<Todo>]
 }
